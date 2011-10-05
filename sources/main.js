cc8c2fd6cbcfe66c38d2da8cdd3ad9eef06f7c05
Ciprian Dorin, Craciun <ciprian@volution.ro>
2011-10-05 11:25:35 +0300

    Detect and expose URLs in success messages.

--- sources/main.js
+++ sources/main.js
@@ -39,13 +39,14 @@ function _main ()
 dust.filters.json = function (_value) {
 	return (JSON.stringify (_value));
 };
-
 dust.filters.jsonp = function (_value) {
 	return (JSON.stringify (_value, null, 4));
 };
-
 dust.filters.htmlpre = function (_value) {
 	return (_value.replace (/</g, "&lt;") .replace (/>/g, "&gt;"));
+};
+dust.filters.insa = function (_value) {
+	return (_value.replace (/((http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?)/g, "<a href='$1'>$1</a>"));
 };
 
 // ---------------------------------------
