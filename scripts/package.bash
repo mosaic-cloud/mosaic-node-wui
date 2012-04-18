8289262ba8ee4a474c34d0be2492878d85f35695
Ciprian Dorin, Craciun <ciprian@volution.ro>
2012-04-18 19:22:31 +0300

    Make NodeJS package version explicit

--- scripts/package.bash
+++ scripts/package.bash
@@ -110,7 +110,7 @@ cat >"${_outputs}/package/pkg.json" <<EOS
 	"description" : "mOSAIC Examples: Realtime Feeds",
 	"directories" : [ "bin", "lib" ],
 	"depends" : [
-		"mosaic-nodejs",
+		"mosaic-nodejs-0.6.15",
 		"libxml2"
 	]
 }
