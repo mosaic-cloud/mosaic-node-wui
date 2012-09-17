27f25cbd52b8c2d8bef5dabd48b45f9a56432c96
Ciprian Dorin Craciun <ciprian@volution.ro>
2012-09-17 20:10:47 +0300

    Query controller for component types

--- sources/schemas.js
+++ sources/schemas.js
@@ -5,21 +5,26 @@ if (require.main === module)
 
 // ---------------------------------------
 
+var _ = require ("underscore");
 var child_process = require ("child_process");
 var fs = require ("fs");
 var path = require ("path");
 
 var configuration = require ("./configuration");
 var transcript = require ("./transcript") (module, configuration.libTranscriptLevel);
+var controller = require ("./controller");
 
 // ---------------------------------------
 
 var _schemasPy = path.join (path.dirname (module.filename), "schemas.py");
 var _schemasPyInterval = 500;
+var _schemasPyCache = null;
 var _schemasJsonPath = path.join (path.dirname (module.filename), "schemas.json");
 var _schemasYamlPath = path.join (path.dirname (module.filename), "schemas.yaml");
 
-function _schemasRefresh () {
+// ---------------------------------------
+
+function _schemasPyRefresh () {
 	var _jsonTimestamp = fs.statSync (_schemasJsonPath) .mtime;
 	var _yamlTimestamp = fs.statSync (_schemasYamlPath) .mtime;
 	if (_jsonTimestamp < _yamlTimestamp) {
@@ -28,23 +33,75 @@ function _schemasRefresh () {
 		var _child = child_process.spawn ("python2", [_schemasPy], {cwd : process.cwd (), env : process.env, customFds : [_devNull, _devNull, process.stderr]});
 		fs.close (_devNull)
 		_child.on ("exit", function (_code) {
-			if (_code == 0)
-				setTimeout (_schemasRefresh, _schemasPyInterval);
-			else {
+			if (_code == 0) {
+				setTimeout (_schemasPyRefresh, _schemasPyInterval);
+				_schemasPyLoad ();
+			} else {
 				transcript.traceError ("`schemas.yaml` seems to be invalid; rescheduling...");
-				setTimeout (_schemasRefresh, _schemasPyInterval * 4);
+				setTimeout (_schemasPyRefresh, _schemasPyInterval * 4);
 			}
 		});
 	} else {
-		setTimeout (_schemasRefresh, _schemasPyInterval);
+		setTimeout (_schemasPyRefresh, _schemasPyInterval);
 	}
 }
 
-_schemasRefresh ();
-
-function _processes () {
+function _schemasPyLoad () {
 	var _schemas = fs.readFileSync (_schemasJsonPath, "utf8");
 	_schemas = JSON.parse (_schemas);
+	_schemasPyCache = _schemas;
+	_schemasCache = null;
+}
+
+_schemasPyLoad ();
+_schemasPyRefresh ();
+
+// ---------------------------------------
+
+var _schemasCtlInterval = 1000;
+var _schemasCtlCache = null;
+
+function _schemasCtlRefresh () {
+	controller.getConfigurators (function (_error, _outcome) {
+		if (_error == null) {
+			if (_outcome.ok) {
+				setTimeout (_schemasCtlRefresh, _schemasCtlInterval);
+				var _schemas = {};
+				_ (_outcome.configurators) .each (function (_configurator) {
+					_schemas[_configurator.type] = {
+							configurationTemplate : null,
+							callOperations : {},
+							castOperations : {},
+					};
+				});
+				_schemasCtlCache = _schemas;
+				_schemasCache = null;
+			} else {
+				transcript.traceError ("controller failed; rescheduling...");
+				setTimeout (_schemasCtlRefresh, _schemasCtlInterval * 4);
+			}
+		} else {
+			transcript.traceError ("controller failed; rescheduling...");
+			setTimeout (_schemasCtlRefresh, _schemasCtlInterval * 4);
+		}
+	});
+}
+
+_schemasCtlRefresh ();
+
+// ---------------------------------------
+
+var _schemasCache = null;
+
+function _processes () {
+	var _schemas = _schemasCache;
+	if (_schemas == null) {
+		_schemas = {};
+		if (_schemasCtlCache != null)
+			_ (_schemas) .extend (_schemasCtlCache);
+		if (_schemasPyCache != null)
+			_ (_schemas) .extend (_schemasPyCache);
+	}
 	return (_schemas);
 }
 
