cd56a0d33222180923ad2ad9f51b73c868f1e89d
Ciprian Dorin Craciun <ciprian@volution.ro>
2014-11-17 21:16:47 +0200

    Minor script updates

--- scripts/run-service.bash
+++ scripts/run-service.bash
@@ -1,19 +1,21 @@
 #!/dev/null
 
-if ! test "${#}" -le 1 ; then
+if ! test "${#}" -eq 0 ; then
 	echo "[ee] invalid arguments; aborting!" >&2
 	exit 1
 fi
 
-_identifier="${1:-0000000054d5b2b2b06341e1a788a2eea3733b90}"
+_identifier="${mosaic_service_identifier:-0000000054d5b2b2b06341e1a788a2eea3733b90}"
 
+## chunk::be1d894b132982aa08a6adf7e406c9a9::begin ##
 if test -n "${mosaic_service_temporary:-}" ; then
 	_tmp="${mosaic_service_temporary:-}"
 elif test -n "${mosaic_temporary:-}" ; then
 	_tmp="${mosaic_temporary}/services/${_identifier}"
 else
-	_tmp="${TMPDIR:-/tmp/mosaic}/services/${_identifier}"
+	_tmp="${TMPDIR:-/tmp}/mosaic/services/${_identifier}"
 fi
+## chunk::be1d894b132982aa08a6adf7e406c9a9::end ##
 
 _node_env+=(
 		mosaic_service_identifier="${_identifier}"
@@ -46,20 +48,17 @@ _node_args+=(
 		"${_node_sources}/main.js"
 )
 
+## chunk::da43d7ef47da796de30612bd22b4e475::begin ##
 mkdir -p -- "${_tmp}"
 cd -- "${_tmp}"
 
-if test "${mosaic_service_restart:-false}" != true ; then
-	exec env -i "${_node_env[@]}" "${_node_bin}" "${_node_args[@]}"
-else
-	while true ; do
-		_outcome=0
-		env -i "${_node_env[@]}" "${_node_bin}" "${_node_args[@]}" || _outcome="${?}"
-		if test "${_outcome}" -ne 2 ; then
-			break
-		fi
-	done
-	exit "${_outcome}"
+exec {_lock}<"${_tmp}"
+if ! flock -x -n "${_lock}" ; then
+	echo '[ee] failed to acquire lock; aborting!' >&2
+	exit 1
 fi
+## chunk::da43d7ef47da796de30612bd22b4e475::end ##
+
+exec env -i "${_node_env[@]}" "${_node_bin}" "${_node_args[@]}"
 
 exit 1
